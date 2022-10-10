b47cdf70db1e633164ebadfcbb6e0fba0521c650
igorbr <igor.bronovytskyi@playtech.com>
2022-10-10 13:19:11 +0300

    browser_api fix

--- browser_api/js/browser_api.js
+++ browser_api/js/browser_api.js
@@ -12,12 +12,12 @@ document.querySelector('.b2').addEventListener('click', function() {
 
 //-----button 3
 document.querySelector('.b3').addEventListener('click', function() {
-    document.querySelector('.t3').firstChild.href = 'https://developer.mozilla.org/ru/docs/Web/API';
+    document.querySelector('.t3 a').href = 'https://developer.mozilla.org/ru/docs/Web/API';
 });
 
 //-----button 4
 document.querySelector('.b4').addEventListener('click', function() {
-    document.querySelector('img').src = 'images/bro.jpg';
+    document.querySelector('.t4 img').src = 'images/bro.jpg';
 });
 
 //-----button 5
@@ -32,7 +32,7 @@ document.querySelector('.b6').addEventListener('click', function() {
 
 //-----button 7
 document.querySelector('.b7').addEventListener('click', function() {
-    document.querySelector('.t7').innerHTML = '<span>Тест <i>Цветы</i></span>'
+    document.querySelector('.t7').innerHTML = '<span>Тест <i>Цветы</i></span>';
 });
 
 //-----button 8
@@ -67,12 +67,15 @@ document.querySelector('.b13').addEventListener('click', function() {
 
 //-----task 14
 window.addEventListener('resize', function() {
-    document.querySelector('.t99').style.backgroundColor = 'rgb(' + (Math.round(Math.random() * 255)) + ', ' + (Math.round(Math.random() * 255)) + ', ' + (Math.round(Math.random() * 255)) + ')';
+    var red = Math.round(Math.random() * 255);
+    var green = Math.round(Math.random() * 255);
+    var blue = Math.round(Math.random() * 255);
+    document.querySelector('.t99').style.backgroundColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
 });
 
 //-----count
-document.querySelector('.t98 input').addEventListener('input', function() {
-    document.querySelector('.t98-2').innerText = document.querySelector('.t98 input').value.length;
+document.querySelector('.t98 input').addEventListener('input', function(e) {
+    document.querySelector('.t98-2').innerText = e.currentTarget.value.length;
 });
 
 //-----whore2
@@ -126,7 +129,6 @@ document.querySelector('.b14').addEventListener('click', function() {
     whore2.appendChild(teaser);
 
     document.querySelector('.whores-container').appendChild(whore2);
-
 });
 
 document.querySelector('.b14').addEventListener('click', function() {
@@ -147,142 +149,95 @@ document.querySelector('.b14').addEventListener('click', function() {
 
 //-----task fish
 document.querySelector('.b15').addEventListener('click', function() {
+    var fish = document.querySelector('.fish');
+
     if (document.querySelector('.cat-container-1 .fish' )) {
-        // document.querySelector('.cat-container-1').removeChild(document.querySelector('.fish'));
-        document.querySelector('.cat-container-2').appendChild(document.querySelector('.fish'));
+        document.querySelector('.cat-container-2').appendChild(fish);
     } else {
-        // document.querySelector('.cat-container-2').removeChild(document.querySelector('.fish'));
-        document.querySelector('.cat-container-1').appendChild(document.querySelector('.fish'));
+        document.querySelector('.cat-container-1').appendChild(fish);
     }
 });
 
 //------ task tooth
 document.querySelector('.b16').addEventListener('click', function() {
-    // if (document.querySelector('.tooth-container .tooth' )) {
-        document.querySelector('.tooth-container').removeChild(document.querySelector('.tooth'));
-    // }
+    var tooth = document.querySelector('.tooth');
+
+    if (tooth) {
+        document.querySelector('.tooth-container').removeChild(tooth);
+        //tooth.remove();
+    }
 });
 
 //-----skier
 document.querySelector('.b17').addEventListener('click', function() {
-    var obj = document.querySelector('.skier');
-    var distance = 0;
-    var tempo = setInterval(function() {
-        distance += 5;
-        obj.style.left = distance + 'px';
+    var skier = document.querySelector('.skier');
+    var x = 0;
+
+    var intervalId = setInterval(function() {
+        x += 5;
+        skier.style.left = x + 'px';
     }, 16);
 
     document.querySelector('.b17-2').addEventListener('click', function() {
-       clearInterval(tempo);
+       clearInterval(intervalId);
     });
 });
 
 //-----Mikki
-var drawMouse = function(coordinates) {
-    coordinates.forEach(str_element => {
-        for(var i = 0; i < str_element.length; i++) {
-            if (str_element[i] === 'X') {
-                // draw black tile
-                var blackTile = document.createElement('div');
-                blackTile.classList.add('mikki_tile');
-                blackTile.style.backgroundColor = 'black';
-                document.querySelector('.mikki_tiles').appendChild(blackTile);
-            } else {
-                // draw white tile
-                var whiteTile = document.createElement('div');
-                whiteTile.classList.add('mikki_tile');
-                whiteTile.style.boxSizing = 'border-box';
-                whiteTile.style.border = '1px solid gray';
-                document.querySelector('.mikki_tiles').appendChild(whiteTile);
-            }
+for (var i = 0; i< mikki.length; i++) {
+    for (var j = 0; j< mikki[i].length; j++) {
+        var tile = document.createElement('div');
+        tile.classList.add('mikki_tile');
+        if (mikki[i][j] === 'X') {
+            tile.style.backgroundColor = 'black';
         }
-
-    });
+        document.querySelector('.mikki_tiles').appendChild(tile);
+    }
 }
-drawMouse(mikki)
 
 //-----Mario World
 document.querySelector('.b18').addEventListener('click', function() {
-    var i = 0;
-    while (i < map.length) {
-        var j = 0;
-        while (j < map[i].length) {
-            var element = document.createElement('div');
-            element.classList.add('tile');
-            element.style.top = (i*16) + 'px';
-            element.style.left = (j*16) + 'px';
+    for (var i = 0; i < map.length; i++) {
+        for (var j = 0; j < map[i].length; j++) {
+            var tile = document.createElement('div');
+            tile.classList.add('tile');
+            tile.style.top = i * 16 + 'px';
+            tile.style.left = j * 16 + 'px';
+
             if (map[i][j] === 'z') {
-                element.classList.add('x_z');
+                tile.classList.add('x_z');
             }
-            document.querySelector('.scene').appendChild(element);
 
-            var element2 = document.createElement('div');
-            element2.classList.add('tile');
-            element2.style.top = (i*16) + 'px';
-            element2.style.left = (j*16) + 'px';
             if (map[i][j] === 'k') {
-                element.classList.add('x_k');
+                tile.classList.add('x_k');
             }
-            document.querySelector('.scene').appendChild(element2);
 
-            var element3 = document.createElement('div');
-            element3.classList.add('tile');
-            element3.style.top = (i*16) + 'px';
-            element3.style.left = (j*16) + 'px';
             if (map[i][j] === 'c') {
-                element.classList.add('x_c');
+                tile.classList.add('x_c');
             }
-            document.querySelector('.scene').appendChild(element3);
 
-            var element4 = document.createElement('div');
-            element4.classList.add('tile');
-            element4.style.top = (i*16) + 'px';
-            element4.style.left = (j*16) + 'px';
             if (map[i][j] === 't') {
-                element.classList.add('x_t');
+                tile.classList.add('x_t');
             }
-            document.querySelector('.scene').appendChild(element4);
 
-            var element5 = document.createElement('div');
-            element5.classList.add('tile');
-            element5.style.top = (i*16) + 'px';
-            element5.style.left = (j*16) + 'px';
             if (map[i][j] === 'g') {
-                element.classList.add('x_g');
+                tile.classList.add('x_g');
             }
-            document.querySelector('.scene').appendChild(element5);
 
-            var element6 = document.createElement('div');
-            element6.classList.add('tile');
-            element6.style.top = (i*16) + 'px';
-            element6.style.left = (j*16) + 'px';
             if (map[i][j] === 'b') {
-                element.classList.add('x_b');
+                tile.classList.add('x_b');
             }
-            document.querySelector('.scene').appendChild(element6);
-
-            var element7 = document.createElement('div');
-            element7.classList.add('tile');
-            element7.style.top = (i*16) + 'px';
-            element7.style.left = (j*16) + 'px';
             if (map[i][j] === 'd') {
-                element.classList.add('x_d');
+                tile.classList.add('x_d');
             }
-            document.querySelector('.scene').appendChild(element7);
-
-            var element8 = document.createElement('div');
-            element8.classList.add('tile');
-            element8.style.top = (i*16) + 'px';
-            element8.style.left = (j*16) + 'px';
             if (map[i][j] === 'w') {
-                element.classList.add('x_w');
+                tile.classList.add('x_w');
             }
-            document.querySelector('.scene').appendChild(element8);
-            j++;
+
+            document.querySelector('.scene').appendChild(tile);
         }
-        i++;
     }
- });
+});
 
 //------Slider
 document.querySelector('.pagination .next').addEventListener('click', function(){
@@ -303,35 +258,34 @@ document.querySelector('.pagination .previous').addEventListener('click', functi
 
 //------Questions
 var questions = document.querySelectorAll('.question');
-var i = 0;
-while (i < questions.length) {
-    questions[i].addEventListener('click', function() {
-        this.classList.add('active');
+questions.forEach(function(question) {
+    question.addEventListener('click', function() {
+        this.classList.toggle('active');
     });
-    i++;
-}
+});
 
 //------ Tabs
 document.querySelector('.tabs').addEventListener('click', function(e) {
+    if (!e.target.classList.contains('item')) {
+        return;
+    }
+
     document.querySelector('.tabs .active').classList.remove('active');
     document.querySelector('.tabs-content .active').classList.remove('active');
+
     e.target.classList.add('active');
-    var list = document.querySelectorAll('.tabs-content .item');
-    list.forEach(tabContentItem => {
-        if(tabContentItem.dataset.tab === e.target.dataset.tab) {
-            tabContentItem.classList.add('active');
-        }
-    })
+    document.querySelector('.tabs-content .item[data-tab="' + e.target.dataset.tab + '"]').classList.add('active');
 });
 
 //------Login
 document.querySelector('.show-login-pop-up').addEventListener('click', function() {
-    document.querySelector('.overlay').classList.remove('hidden'); //чому не можна знайти по .overlay .hidden
+    document.querySelector('.overlay').classList.remove('hidden');
     document.querySelector('.pop-up').classList.remove('hidden');
-    document.querySelector('.close').addEventListener('click', function() {
-        document.querySelector('.overlay').classList.add('hidden');
-        document.querySelector('.pop-up').classList.add('hidden');
-    })
+});
+
+document.querySelector('.close').addEventListener('click', function() {
+    document.querySelector('.overlay').classList.add('hidden');
+    document.querySelector('.pop-up').classList.add('hidden');
 });
 
 // ------Prevent
@@ -342,5 +296,9 @@ document.querySelector('.link-ebanoe').addEventListener('click', function(e) {
 //-----Spam
 document.querySelector('.t97 .b97').addEventListener('click', function() {
     document.querySelector('.t97').style.display = 'none';
-    localStorage.setItem('attentionSpam', 'alarm');
+    localStorage.setItem('hideAd', 'true');
 });
+
+if (localStorage.getItem('hideAd') === 'true') {
+    document.querySelector('.t97').style.display = 'none';
+}
