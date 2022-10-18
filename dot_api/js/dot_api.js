d1463f759f99bea716ab4c547c542b73181ac936
igorbr <igor.bronovytskyi@playtech.com>
2022-10-18 14:07:56 +0300

    dot fix

--- dot_api/js/dot_api.js
+++ dot_api/js/dot_api.js
@@ -2,30 +2,30 @@
 console.log('Hello world!');
 
 //---------Game
-document.querySelector('.add-games').addEventListener('click', function () {
+document.querySelector('.add-games').addEventListener('click', function() {
     var gameTemplate = document.querySelector('#game-template').innerHTML;
-    document.querySelector('.games').innerHTML += doT.template(gameTemplate)({games: games});
-
+    document.querySelector('#games').innerHTML = doT.template(gameTemplate)({games: games});
 });
 
 //---------Menu
-document.querySelector('.btn-nav-en').addEventListener('click', function () {
-    var NavTemplate = document.querySelector('#nav-template').innerHTML;
+document.querySelector('.btn-nav-en').addEventListener('click', function() {
+    var navTemplate = document.querySelector('#nav-template').innerHTML;
+    document.querySelector('#nav').innerHTML = doT.template(navTemplate)(navEn);
+});
 
-    document.querySelector('.nav').innerHTML += doT.template(NavTemplate)(navEn);
-
+document.querySelector('.btn-nav-ru').addEventListener('click', function() {
+    var navTemplate = document.querySelector('#nav-template').innerHTML;
+    document.querySelector('#nav').innerHTML = doT.template(navTemplate)(navRu);
 });
 
 //-------CVs
-document.querySelector('.add-whores').addEventListener('click', function () {
+document.querySelector('.add-whores').addEventListener('click', function() {
     var whoreTemplate = document.querySelector('#whore-template').innerHTML;
-    document.querySelector('.whores').innerHTML += doT.template(whoreTemplate)({whores: whores});
-
+    document.querySelector('#whores').innerHTML = doT.template(whoreTemplate)({whores: whores});
 });
 
 //--------Movies
-document.querySelector('.add-movies').addEventListener('click', function () {
+document.querySelector('.add-movies').addEventListener('click', function() {
     var movieTemplate = document.querySelector('#movie-template').innerHTML;
-    document.querySelector('.movies').innerHTML += doT.template(movieTemplate)({movies: movies.slice(5)});
-
+    document.querySelector('#movies').innerHTML = doT.template(movieTemplate)({movies: movies.slice(5)});
 });
