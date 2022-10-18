528a05ac1e821625cefd43a073b404f3007be3ea
Iuliiao <juliette.bam@gmail.com>
2022-10-18 13:23:27 +0300

    DoT_api questions

--- dot_api/js/dot_api.js
+++ dot_api/js/dot_api.js
@@ -1,2 +1,31 @@
 // Write your code here
 console.log('Hello world!');
+
+//---------Game
+document.querySelector('.add-games').addEventListener('click', function () {
+    var gameTemplate = document.querySelector('#game-template').innerHTML;
+    document.querySelector('.games').innerHTML += doT.template(gameTemplate)({games: games});
+
+});
+
+//---------Menu
+document.querySelector('.btn-nav-en').addEventListener('click', function () {
+    var NavTemplate = document.querySelector('#nav-template').innerHTML;
+
+    document.querySelector('.nav').innerHTML += doT.template(NavTemplate)(navEn);
+
+});
+
+//-------CVs
+document.querySelector('.add-whores').addEventListener('click', function () {
+    var whoreTemplate = document.querySelector('#whore-template').innerHTML;
+    document.querySelector('.whores').innerHTML += doT.template(whoreTemplate)({whores: whores});
+
+});
+
+//--------Movies
+document.querySelector('.add-movies').addEventListener('click', function () {
+    var movieTemplate = document.querySelector('#movie-template').innerHTML;
+    document.querySelector('.movies').innerHTML += doT.template(movieTemplate)({movies: movies.slice(5)});
+
+});
