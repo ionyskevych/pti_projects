04b29a4b12c2d4b98f91b0da1d64d5182bf5f63a
Iuliiao <juliette.bam@gmail.com>
2022-12-02 10:51:40 +0200

    http and newJS

--- http/js/http.js
+++ http/js/http.js
@@ -1,4 +1,88 @@
 // Write your code here
-console.log('Hello world!');
 
-// Чередуй два способа (XMLHttpRequest и $.ajax)
+var ajax = function(method, url, data, callback) {
+    var xhr = new XMLHttpRequest;
+    xhr.open(method, url);
+    xhr.addEventListener('readystatechange', function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                callback(xhr.responseText);
+            } else {
+                console.log('Помилка!!!!!');
+            }
+        }
+    });
+    xhr.send(method === 'GET' ? null : data);
+};
+
+// ------------------ Games
+
+$('.add-games').on('click', function() {
+    ajax('GET', 'http://127.0.0.1:3001/games', null, function(body) {
+        var games = JSON.parse(body);
+        var gameTemplate = $('#game-template').html();
+        $('#games').html(doT.template(gameTemplate)({games: games}));
+    });
+});
+
+//-------------------- Menu
+
+$('.btn-nav-en').on('click', function () {
+    $.ajax({
+        method: 'GET',
+        url: 'http://127.0.0.1:3001/menu/en',
+        dataType: 'json'
+    }).done(function (body) {
+        console.log('success', body);
+        var navTemplate = document.querySelector('#nav-template').innerHTML;
+        document.querySelector('#nav').innerHTML = doT.template(navTemplate)(body);
+    }).fail(function () {
+        console.log('error');
+    }).always(function () {
+        console.log('complete');
+    });
+});
+
+$('.btn-nav-ru').on('click', function () {
+    $.ajax({
+        method: 'GET',
+        url: 'http://127.0.0.1:3001/menu/ru',
+        dataType: 'json'
+    }).done(function (body) {
+        console.log('success', body);
+        var navTemplate = document.querySelector('#nav-template').innerHTML;
+        document.querySelector('#nav').innerHTML = doT.template(navTemplate)(body);
+    }).fail(function () {
+        console.log('error');
+    }).always(function () {
+        console.log('complete');
+    });
+});
+
+//------------- Whores
+
+$('.add-whores').on('click', function() {
+    ajax('GET', 'http://127.0.0.1:3001/whores', null, function(body) {
+        var whores = JSON.parse(body);
+        var whoreTemplate = $('#whore-template').html();
+        $('#whores').html(doT.template(whoreTemplate)({whores: whores}));
+    });
+});
+
+//------------- Movies
+
+$('.add-movies').on('click', function() {
+    $.ajax({
+        method: 'GET',
+        url: 'http://127.0.0.1:3001/movies',
+        dataType: 'json'
+    }).done(function (body) {
+        console.log('success', body);
+        var gameTemplate = $('#movie-template').html();
+        $('#movies').html(doT.template(gameTemplate)({movies: body}));
+    }).fail(function () {
+        console.log('error');
+    }).always(function () {
+        console.log('complete');
+    });
+});
--- js/es6plus.js
+++ js/es6plus.js
@@ -6,6 +6,9 @@
 // };
 
 // TODO: Write your code here
+var f = function(a=7, b=4) {
+    console.log(a, b);
+};
 
 
 
@@ -36,6 +39,19 @@ var jasmine = {
 //     '</div>';
 
 // TODO: Write your code here
+var whore = `
+    <div class="whore">
+        <div class="whore-name"> ${jasmine.name}</div>
+        <img src="${jasmine.photo}" width="200">
+        <div class="whore-age">Возраст: ${jasmine.age}</div>
+        <div class="whore-boobs">Размер груди: ${jasmine.boobs}</div>
+        <div class="whore-height">Рост: ${jasmine.height} </div>
+        <div class="whore-weight"> Вес: ${jasmine.weight}</div>
+        <div class="whore-phone">${jasmine.phone}</div>
+        <div class="whore-can-come"> Выезд: ${(jasmine.can_come ? '+' : '-')}</div>
+        <div class="whore-teaser">${jasmine.teaser}</div>
+    </div>
+    `;
 
 
 
@@ -48,7 +64,13 @@ var name = 'Вася';
 // };
 
 // TODO: Write your code here
-
+var o = {
+    name: name,
+    getName() {
+        return this.name;
+    }
+};
+o.getName();
 
 
 // Перепиши выражения используя оператор ...
@@ -77,10 +99,21 @@ var vasya = {
 // TODO: Write your code here
 
 
+whores = [...whores, {name: 'Ванесса', age: 22 }];
+
+var fruits3 = [...fruits1, ...fruits2];
+
+var person = {...vasya, ...{age: 31}};
+
+
+
+
+
 
 // Перепиши определения функций используя стрелочные функции
 // var f1 = function() {};
 // var f2 = function(x) { return x * x; };
+
 // var f3 = function(a, b) {
 //     var c = a + b;
 //     return c * c;
@@ -88,14 +121,41 @@ var vasya = {
 // var f4 = function() { return 'Хуй мусорам!'; };
 
 // TODO: Write your code here
+var f1 = () => {};
+var f2 = (x) => x * x;
 
+var f3 = (a, b) => {
+    var c = a + b;
+    return c * c;
+};
 
+var f4 = () => 'Хуй мусорам!'
 
 // Перепиши класс Circle из файла js/functions_part_4.js используя синтаксис классов
 
 // TODO: Write your code here
 
+class Circle {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    getDiameter() {
+        return 2 * this.radius;
+    }
+    getPerimeter() {
+        return this.getDiameter() * 3.14;
+    }
+    getSquare() {
+        return this.radius * this.radius * 3.14;
+    }
+    static cool() {
+        console.log('cool');
+    }
+};
 
+var circle = new Circle(5, 5, 5);
 
 // Перепиши инструкции определения переменных используя деструктурирующее присваивание
 var config = {
@@ -110,4 +170,5 @@ var config = {
 
 // TODO: Write your code here
 
+var {host, port, dbName = 'default'} = config;
 
